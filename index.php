8aa7171b61233734e8b1e68beb55bc60e12cd910
77-ahmad-collab <63909388+77-ahmad-collab@users.noreply.github.com>
2020-08-30 23:57:14 +0500

    adding

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+        $var1 = 20;
+        $var2 = 20;
+        echo "the values are :";
+        echo "<br>";
+        echo $var1 + $var2;
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>my filte</div>
+    
+</body>
+</html>
